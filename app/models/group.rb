c5051ec57c3e2a0fb6e2b4a640d6b61953f29be4
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-03 12:48:26 +0300

    Don't allow creating duplicate members

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,7 +4,7 @@ class Group < ActiveRecord::Base
   validates :name, presence: true
 
   def join(user)
-    memberships.create(user: user) if user
+    memberships.where(user: user).first_or_create if user
   end
 
   def leave(user)
--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -19,5 +19,28 @@ RSpec.describe GroupsController do
       expect(@group.admin?(@user)).to be true
     end
   end
+
+  describe 'POST /groups/:id/join' do
+
+    before do
+      @group = Group.create!(name: 'test')
+      @user = User.create!(email: 'other@example.com', password: 'otherother')
+      sign_in @user
+      post :join, id: @group
+    end
+
+    it 'redirects to the group' do
+      expect(response).to redirect_to(group_path(@group))
+    end
+    it 'makes the current user a member' do
+      expect(@group.member(@user)).not_to be_nil
+    end
+    it 'does not allow to joint twice' do
+      post :join, id: @group
+      expect(@group.memberships.where(user: @user).count).to eq(1)
+    end
+
+  end
+
 end
 
