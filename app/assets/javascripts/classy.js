ed599167435cf48c90ee6ad0ae87eb619885e9e4
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-08 18:28:48 +0300

    4 space indents for non-ruby code

--- app/assets/javascripts/classy.js
+++ app/assets/javascripts/classy.js
@@ -1,29 +1,27 @@
-
 function beClassy(schema) {
-  return function(element, options) {
-    var cl = element.classList;
-    for (var key in schema) {
-      if (!schema.hasOwnProperty(key)) continue;
+    return function(element, options) {
+        var cl = element.classList;
+        for (var key in schema) {
+            if (!schema.hasOwnProperty(key)) continue;
 
-      var classes = schema[key];
-      var val = options[key] || 0;
-      if (Array.isArray(classes)) {
-        var index = val | 0;
-        for (var i = 0; i < classes.length; i++) {
-          if (i == index) {
-            cl.add(classes[i]);
-          } else {
-            cl.remove(classes[i]);
-          }
+            var classes = schema[key];
+            var val = options[key] || 0;
+            if (Array.isArray(classes)) {
+                var index = val | 0;
+                for (var i = 0; i < classes.length; i++) {
+                    if (i == index) {
+                        cl.add(classes[i]);
+                    } else {
+                        cl.remove(classes[i]);
+                    }
+                }
+            } else {
+                if (val) {
+                    cl.add(classes);
+                } else {
+                    cl.remove(classes);
+                }
+            }
         }
-      } else {
-        if (val) {
-          cl.add(classes);
-        } else {
-          cl.remove(classes);
-        }
-      }
     }
-  }
 }
-
--- app/assets/javascripts/http.js
+++ app/assets/javascripts/http.js
@@ -1,74 +1,74 @@
 
 function HTTP() {
-  this.globalHeaders = [];
+    this.globalHeaders = [];
 }
 
 HTTP.prototype.request = function(method, url, body, options) {
-  options = options || { };
-  var success = options.success || _.noop;
-  var error = options.error || _.noop;
-  var headers = options.headers || { };
-  var auth = options.auth || { user: '', password: '' };
+    options = options || { };
+    var success = options.success || _.noop;
+    var error = options.error || _.noop;
+    var headers = options.headers || { };
+    var auth = options.auth || { user: '', password: '' };
 
-  var xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
 
-  xhr.open(method, url, true, auth.user, auth.password);
-  
-  _.forOwn(headers, function (value, key) {
-    if (_.isArray(value)) {
-      _.forEach(value, function(val) {
-        xhr.setRequestHeader(key, val);
-      });
+    xhr.open(method, url, true, auth.user, auth.password);
+
+    _.forOwn(headers, function (value, key) {
+        if (_.isArray(value)) {
+            _.forEach(value, function(val) {
+                xhr.setRequestHeader(key, val);
+            });
+        } else {
+            xhr.setRequestHeader(key, value);
+        }
+    });
+
+    _.forEach(this.globalHeaders, function(header) {
+        xhr.setRequestHeader(header[0], header[1]);
+    });
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState != 4) {
+            return;
+        }
+        if (xhr.status >= 200 && xhr.status < 300) {
+            success(xhr);
+        } else {
+            error(xhr);
+        }
+    };
+
+    if (body) {
+        xhr.send(body);
     } else {
-      xhr.setRequestHeader(key, value);
+        xhr.send();
     }
-  });
 
-  _.forEach(this.globalHeaders, function(header) {
-    xhr.setRequestHeader(header[0], header[1]);
-  });
-
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState != 4) {
-      return;
-    }
-    if (xhr.status >= 200 && xhr.status < 300) {
-      success(xhr);
-    } else {
-      error(xhr);
-    }
-  };
-
-  if (body) {
-    xhr.send(body);
-  } else {
-    xhr.send();
-  }
-  
-  return xhr;
+    return xhr;
 };
 
 HTTP.prototype.get = function(url, options) {
-  this.request('GET', url, null, options);
+    this.request('GET', url, null, options);
 };
 HTTP.prototype.post = function(url, body, options) {
-  this.request('POST', url, body, options);
+    this.request('POST', url, body, options);
 };
 HTTP.prototype.patch = function(url, body, options) {
-  this.request('PATCH', url, body, options);
+    this.request('PATCH', url, body, options);
 };
 HTTP.prototype.delete_ = function(url, options) {
-  this.request('DELETE', url, null, options);
+    this.request('DELETE', url, null, options);
 };
 
 HTTP.prototype.postJson = function(url, body, options) {
-  this.post(url, JSON.stringify(body), options);
+    this.post(url, JSON.stringify(body), options);
 };
 HTTP.prototype.patchJson = function(url, body, options) {
-  this.patch(url, JSON.stringify(body), options);
+    this.patch(url, JSON.stringify(body), options);
 };
 
 HTTP.prototype.globalHeader = function(header, value) {
-  this.globalHeaders.push([header, value]);
+    this.globalHeaders.push([header, value]);
 }
 
