4fb2638e2c52151cc81ff08e322f4614ca67cb28
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-14 20:11:50 +0300

    Single setter for classy

--- app/assets/javascripts/classy.js
+++ app/assets/javascripts/classy.js
@@ -1,18 +1,37 @@
 function beClassy(schema) {
-    var getter = function(key) {
-        return function(element) {
+    var attr = function(key) {
+        return function(element, value) {
             var cl = element.classList;
-            
             var classes = schema[key];
-            if (Array.isArray(classes)) {
-                for (var i = 0; i < classes.length; i++) {
-                    if (cl.contains(classes[i])) {
-                        return i;
+
+            if (value !== undefined) {
+                if (Array.isArray(classes)) {
+                    var index = value | 0;
+                    for (var i = 0; i < classes.length; i++) {
+                        if (i == index) {
+                            cl.add(classes[i]);
+                        } else {
+                            cl.remove(classes[i]);
+                        }
+                    }
+                } else {
+                    if (value) {
+                        cl.add(classes);
+                    } else {
+                        cl.remove(classes);
                     }
                 }
-                return -1;
             } else {
-                return cl.contains(classes);
+                if (Array.isArray(classes)) {
+                    for (var i = 0; i < classes.length; i++) {
+                        if (cl.contains(classes[i])) {
+                            return i;
+                        }
+                    }
+                    return -1;
+                } else {
+                    return cl.contains(classes);
+                }
             }
         }
     }
@@ -20,8 +39,8 @@ function beClassy(schema) {
     var func = function(element, options) {
         var cl = element.classList;
 
-        if (options != null) {
-            for (var key in schema) {
+        if (options !== undefined) {
+            for (var key in options) {
                 if (!schema.hasOwnProperty(key)) continue;
 
                 var classes = schema[key];
@@ -66,7 +85,7 @@ function beClassy(schema) {
 
     for (var key in schema) {
         if (!schema.hasOwnProperty(key)) continue;
-        func[key] = getter(key);
+        func[key] = attr(key);
     }
 
     return func;
