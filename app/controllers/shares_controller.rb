92d839aacc35255d0730d1b17e17bbdfa0c22619
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-15 12:52:54 +0300

    Move logic from view to controller

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,3 +1,4 @@
+ShareGroup = Struct.new(:group, :shared, :shareclass)
 
 class SharesController < ApplicationController
   def index
@@ -5,6 +6,22 @@ class SharesController < ApplicationController
     @videos = Video.where(uuid: ids)
     @videos.each do |video|
       authorize video, :share?
+    end
+
+    @share_groups = current_user.groups.map do |group|
+      shared = @videos.map do |video|
+        video.groups.where(id: group.id).count > 0
+      end
+
+      shareclass = if shared.any? { |x| x != shared.first }
+        :mixed # There are both shared and unshared videos
+      elsif shared.first
+        :shared # They are all the same so all must be shared
+      else
+        :unshared # Applies to unshared too
+      end
+
+      ShareGroup.new(group, shared, shareclass)
     end
 
     render
