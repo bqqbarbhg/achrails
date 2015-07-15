a9e73954140fbd9c20628b08331299d2b4d6268d
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-15 13:28:46 +0300

    Add unique index to video uuids

---
+++ db/migrate/20150715101837_add_index_to_video_uuid.rb
@@ -0,0 +1,5 @@
+class AddIndexToVideoUuid < ActiveRecord::Migration
+  def change
+    add_index :videos, :uuid, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713114701) do
+ActiveRecord::Schema.define(version: 20150715101837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,5 +80,7 @@ ActiveRecord::Schema.define(version: 20150713114701) do
     t.integer  "revision",              default: 1, null: false
     t.uuid     "uuid"
   end
+
+  add_index "videos", ["uuid"], name: "index_videos_on_uuid", unique: true, using: :btree
 
 end
