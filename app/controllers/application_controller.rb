bfc3dd9563b2c85f435ceb26a972292e71a305bd
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-22 15:12:15 +0300

    Remove database authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def render_forbidden
     # TODO: Real forbidden
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:learning_layers_oidc]
+  devise :rememberable, :omniauthable,
+    omniauth_providers: [:learning_layers_oidc]
 
   has_many :memberships
   has_many :groups, through: :memberships, source: :group
