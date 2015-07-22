0a8e4873ea282f7cdf626b0b2d857339fe728a31
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-22 15:51:31 +0300

    Don't allow nulls in omniauth columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722115046) do
+ActiveRecord::Schema.define(version: 20150722125026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,8 +54,8 @@ ActiveRecord::Schema.define(version: 20150722115046) do
     t.string   "email",      default: "", null: false
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
-    t.string   "provider"
-    t.string   "uid"
+    t.string   "provider",                null: false
+    t.string   "uid",                     null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
