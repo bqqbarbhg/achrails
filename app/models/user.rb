7d03f7b89140fe1d6f9dfb98974aec756a1dc10b
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-22 16:09:22 +0300

    No need for password anymore

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,6 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      # Remove this when database auth is taken out
-      user.password = Devise.friendly_token[0, 20]
     end
   end
 
