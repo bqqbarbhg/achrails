7bb7b8a89ff0bd55591e64a9e6c7ff9efe6a0067
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-22 17:32:47 +0300

    Remove outdated spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -8,12 +8,6 @@ RSpec.describe HomeController do
     get :index
   end
 
-  context 'not logged in' do
-    it 'redirects to login page' do
-      expect(response).to redirect_to(new_user_session_path)
-    end
-  end
-
   context 'logged in' do
     let(:user) { User.create!(email: 'test@example.com', uid: '1', provider: 'test') }
 
