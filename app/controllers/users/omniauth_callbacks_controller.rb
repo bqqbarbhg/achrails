a8b4952f7fe06e9701b9cfada1e07dd5a55d51e9
bqqbarbhg <samuli.1995@hotmail.com>
2015-07-22 19:05:31 +0300

    Copy paste fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -18,7 +18,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Developer") if is_navigational_format?
     else
-      session["devise.learning_layers_oidc_data"] = request.env["omniauth.auth"]
+      session["devise.developer_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
