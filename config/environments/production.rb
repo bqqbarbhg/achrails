938dd360affb62f609b0f9eb3f6dc57831a399b0
bqqbarbhg <samuli.1995@hotmail.com>
2015-08-17 13:35:15 +0300

    Add default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 end
 
+Rails.application.routes.default_url_options[:host] = 'achrails.herokuapp.com'
+
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 25,
