f13d2351d1f0443f75111467fca58f8e04f0ec0d
bqqbarbhg <samuli.1995@hotmail.com>
2015-08-18 12:34:45 +0300

    Pass group directly to invitation mailer

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,12 +1,7 @@
 class InvitationMailer < ApplicationMailer
-  def invite_email(invitation)
+  def invite_email(invitation, group)
     @email = invitation.expect_email
     @url = invitation_url(invitation)
-    if sss
-      @group = sss.group(invitation.sss_group)
-    else
-      @group = invitation.group
-    end
-    mail to: @email, subject: "You have been invited to: #{@group.name}"
+    mail to: @email, subject: "You have been invited to: #{group.name}"
   end
 end
