380e1f620dc8882bd7f80bc78e4547b2e4a38fb7
bqqbarbhg <samuli.1995@hotmail.com>
2015-08-20 12:34:12 +0300

    Store location to redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,23 @@ class ApplicationController < ActionController::Base
   # checks are mostly done now in policies or per route authenticate_user! calls.
   # before_action :authenticate_user!
 
+
+  after_filter :store_location
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    return unless request.get? 
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
+  end
+
   def render_forbidden
     # TODO: Real forbidden
     render nothing: true, status: :forbidden
@@ -24,7 +41,7 @@ class ApplicationController < ActionController::Base
 
   # Try to return back to the page the login originated from
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || super
+    session[:previous_url] || request.env['omniauth.origin'] || super
   end
 
   def reauthenticate
