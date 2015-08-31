f654bda6a8edc9328492f5b0f19a7b9b90428d1e
bqqbarbhg <samuli.1995@hotmail.com>
2015-08-31 10:25:28 +0300

    Fix images not displaying on Heroku

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,8 @@ module Achrails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Serve assets on Heroku
+    config.serve_static_assets = true
   end
 end
