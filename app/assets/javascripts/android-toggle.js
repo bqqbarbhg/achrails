386c5f597754100026f6dc11cd0daaf3151ae3f6
Matti Jokitulppo <melonmanchan@gmail.com>
2015-11-03 15:12:05 +0200

    Add Android hiding utility javascript

---
+++ app/assets/javascripts/android-toggle.js
@@ -0,0 +1,9 @@
+;$(document).ready(function() {
+    if (true) {
+        $('.hide-on-android').hide();
+        $('.show-on-android').show();
+    } else {
+        $('.hide-on-android').show();
+        $('.show-on-android').hide();
+    }
+});
