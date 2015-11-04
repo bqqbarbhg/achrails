16ef31b57da1429e5d156ca15f6891ac32dbb542
Matti Jokitulppo <melonmanchan@gmail.com>
2015-11-04 09:49:47 +0200

    Enable GZIP compression

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module Achrails
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # Enable gzip compression
+    config.middleware.use Rack::Deflater
     config.i18n.fallbacks = true
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
