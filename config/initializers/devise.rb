0e3c71a9e4b26aaccb812d218a22507a0315fb6a
Matti Jokitulppo <melonmanchan@gmail.com>
2015-11-12 11:16:13 +0200

    Remove prefix from omniauth for testing purposes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -270,8 +270,4 @@ Devise.setup do |config|
   #
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
-
-  prefix = ENV['RAILS_PREFIX'] || '/achrails'
-
-  config.omniauth_path_prefix = prefix
 end
