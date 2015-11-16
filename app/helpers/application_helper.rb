d96d6f47411a528f4b3f613e814061f976369d54
Matti Jokitulppo <melonmanchan@gmail.com>
2015-11-16 10:53:24 +0200

    Sprinkle some more informative page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def full_title(page_title='')
+    base_title = 'Ach So!'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
   def sss
     SSS
   end
