70b449f27460458e0dd4c6e87a0b86c2498d3449
bqqbarbhg <samuli.1995@hotmail.com>
2016-03-10 14:15:37 +0200

    Add Google OAuth2 route

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
   def learning_layers_oidc
     @user = User.from_omniauth(request.env["omniauth.auth"])
     
@@ -44,4 +45,27 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
+
+  def google_oauth2
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.nil?
+      redirect_to oidc_action_error_path(failed_action: "log_in")
+      return
+    end
+
+    if request.env["omniauth.params"]['redirect_to']
+      session = Session.create!
+      request.env["achrails.session_token"] = session.token
+    end
+
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Google+") if is_navigational_format?
+    else
+      session["devise.developer_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
 end
