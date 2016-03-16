42750959ed89e5ddc8c936cdbbf67fcf0cf2c6de
bqqbarbhg <samuli.1995@hotmail.com>
2016-03-16 15:37:10 +0200

    Fix remaining HACK_URIs

--- config/initializers/omniauth_host_patch.rb
+++ config/initializers/omniauth_host_patch.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   module OmniAuth
     module Strategy
       def full_host
-        ENV["HACK_URI"] || ENV["LAYERS_API_URI"].chomp('/')
+        (ENV["ACHRAILS_SELF_URI"] || ENV["LAYERS_API_URI"]).chomp('/')
       end
     end
   end
