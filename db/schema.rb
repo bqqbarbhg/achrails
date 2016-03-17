0079b51a548e1409369e57f64312a94410d06225
bqqbarbhg <samuli.1995@hotmail.com>
2016-03-17 16:48:56 +0200

    Add indexes to sessions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315122655) do
+ActiveRecord::Schema.define(version: 20160317143112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,10 @@ ActiveRecord::Schema.define(version: 20160315122655) do
     t.string   "code"
     t.datetime "expires_at"
   end
+
+  add_index "sessions", ["access_token"], name: "index_sessions_on_access_token", unique: true, using: :btree
+  add_index "sessions", ["code"], name: "index_sessions_on_code", unique: true, using: :btree
+  add_index "sessions", ["refresh_token"], name: "index_sessions_on_refresh_token", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",               default: ""
